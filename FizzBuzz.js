6932ed00aee07944edc128f09fce53529f911298
Holly Knox <holly.knox@softwire.com>
2025-04-22 14:47:06 +0100

    Added function to print Fizz for multiples of 3

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,6 +1,10 @@
 function fizzBuzz() {
     for (let i = 1; i <= 100; i++) {
-        console.log(i);
+        if (i % 3 === 0) {
+            console.log("Fizz");
+        } else {
+            console.log(i);
+        }
     }
 };
 
