8fe6b88d9ff1b278f363bf486f99b1cf849a58df
Holly Knox <holly.knox@softwire.com>
2025-04-22 15:18:20 +0100

    Updated function to print Bang for multiples of 7

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,21 @@
 function fizzBuzz() {
     for (let i = 1; i <= 100; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 === 0) {
-            console.log("Fizz");
+        if (i % 3 === 0) {
+            if (i % 5 === 0) {
+                console.log("FizzBuzz");
+            } else if (i % 7 === 0) {
+                console.log("FizzBang");
+            } else {
+                console.log("Fizz");
+            }
         } else if (i % 5 === 0) {
-            console.log("Buzz");
+            if (i % 7 === 0) {
+                console.log("BuzzBang")
+            } else {
+                console.log("Buzz")
+            }
+        } else if (i % 7 === 0) {
+            console.log("Bang");
         } else {
             console.log(i);
         }
